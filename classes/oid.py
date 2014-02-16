ead0e8c2104d86f38102f6e3be2ff21351e38f76
logan <logan@Latitude.(none)>
2014-02-16 19:43:45 +0000

    oid data class

---
+++ classes/oid.py
@@ -0,0 +1,8 @@
+class OID:
+	"""OID class"""
+
+	def __init__(self):
+		self.hostName = '1.3.6.1.4.1.9.9.23.1.3.4.0'
+		self.neighbors = '1.3.6.1.4.1.9.9.23.1.2.1.1.20'
+		self.vlans = '.1.3.6.1.4.1.9.9.46.1.3.1.1.2'
+		self.macsOnVlan = '.1.3.6.1.2.1.17.4.3.1.1'
