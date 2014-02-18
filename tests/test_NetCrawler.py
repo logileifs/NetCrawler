596dc1c820eb0cf35a9e61f13adfbfed09538fbb
Heimir <heimir@heimir-ubuntu.(none)>
2014-02-18 14:57:52 +0000

    removed unused tests

--- tests/test_NetCrawler.py
+++ tests/test_NetCrawler.py
@@ -4,12 +4,6 @@ import os
 import unittest
 from classes import Crawler
 
-class CodeTests(unittest.TestCase):
+"""class CodeTests(unittest.TestCase):"""
 
-	def test_crawler_port(self):
-		crawler = Crawler(161, '195.218.195.228')
-		assert crawler.getPort() == 161
-
-	def test_crawler_address(self):
-		crawler = Crawler(161, '195.218.195.228')
-		assert crawler.getAddress() == '195.218.195.228'
+	
