fe480b872b65c54b5047d6ae045407ad954cd73b
logan <logan@Latitude.(none)>
2014-02-25 14:56:23 +0000

    fixed tests for parseInput

--- tests/test_Main.py
+++ tests/test_Main.py
@@ -7,23 +7,23 @@ import main
 class MainTests(unittest.TestCase):
 	
 	def test_parseInput_address(self):
-		args = ['', 'a=195.218.195.228', 'p=161', 'c=public']
-		address, port, community = main.parseInput(args)
+		args = ['', 'a=195.218.195.228', 'p=161', 'c=public', 'd']
+		address, port, community, debugMode = main.parseInput(args)
 		assert address == '195.218.195.228'
 	
 	def test_parseInput_port(self):
-		args = ['', 'a=195.218.195.228', 'p=1337', 'c=public']
-		address, port, community = main.parseInput(args)
+		args = ['', 'a=195.218.195.228', 'p=1337', 'c=public', 'd']
+		address, port, community, debugMode = main.parseInput(args)
 		assert port == 1337
 
 	def test_parseInput_defaultPort(self):
-		args = ['', 'a=195.218.195.228', 'c=public']
-		address, port, community = main.parseInput(args)
+		args = ['', 'a=195.218.195.228', 'c=public', 'd']
+		address, port, community, debugMode = main.parseInput(args)
 		assert port == 161
 
 	def test_parseInput_defaultCommunity(self):
-		args = ['', 'a=195.218.195.228', 'p=1337', 'c=public']
-		address, port, community = main.parseInput(args)
+		args = ['', 'a=195.218.195.228', 'p=1337', 'c=public', 'd']
+		address, port, community, debugMode = main.parseInput(args)
 		assert community == 'public'
 	 
 	def test_parseInput_noArguments(self):
