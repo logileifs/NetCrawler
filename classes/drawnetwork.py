744090a2afbc5489b38a4096c7f32d1ce21003d7
Heimir <heimir@heimir-ubuntu.(none)>
2014-02-26 16:52:42 +0000

    Draw the network

---
+++ classes/drawnetwork.py
@@ -0,0 +1,40 @@
+from operator import itemgetter
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+class DrawNetwork:
+    """ To draw the network """
+
+    def draw(network):
+    edges = createEdges(network)
+
+    G=nx.Graph()
+    G.add_nodes_from(network)
+    G.add_edges_from(edges)
+
+    # Draw graph
+    pos=nx.shell_layout(G)
+    nx.draw_spectral(G,node_color='w',with_labels=True)
+    G=nx.dodecahedral_graph()
+
+    # Draw ego as large and red
+    #nx.draw_networkx_nodes(hub_ego,pos,nodelist=[nodes],node_size=300,node_color='r')
+    plt.savefig('network.png')
+    plt.show()
+
+    def createEdges(network):
+    edges = []
+
+    for key, value in network.iteritems():
+            print key, value
+            for neighbor in network[key]['neighbors']:
+                edge = (key, neighbor)
+                edges.append(edge)
+                print neighbor
+
+    for edge in edges:
+        print edge
+
+    return edges
+
