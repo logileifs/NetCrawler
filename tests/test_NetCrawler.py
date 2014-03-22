71e01ca1edddd7f75867fd9ebbd0ada5964ae613
logan <logan@Latitude.(none)>
2014-03-22 16:53:00 +0000

    first test working

--- tests/test_NetCrawler.py
+++ tests/test_NetCrawler.py
@@ -5,17 +5,35 @@
 import unittest
 from classes import netcrawler
 
-"""class CodeTests(unittest.TestCase):"""
 class NetCrawlerTests(unittest.TestCase):
 	"""docstring"""
 
-	def test_constructor(self):
+	def correct_port(self):
+		return 161
+
+	def correct_arguments(self):
 		arg0 = 'netcrawler'
-		arg1 = '192.168.60.254'
+		arg1 = '192.168.60.254/24'
+		arg2 = 161
+		arg3 = 'menandmice'
+		arg4 = False
+
+		return arg0, arg1, arg2, arg3, arg4
+
+	"""arg0 = 'netcrawler'
+	arg1 = '192.168.60.254/24'
+	arg2 = 161
+	arg3 = 'public'
+	arg4 = False"""
+
+	def test_constructor_subnet_argument(self):
+		arg0 = 'netcrawler'
+		arg1 = '192.168.60.254/24'
 		arg2 = 161
 		arg3 = 'public'
 		arg4 = False
-		args = ['', '195.218.195.228', 161, 'public', False]
-		arglist = [161, '192.168.60.254/24', 'public', False]
 		crawler = netcrawler.Crawler(arg0, arg1, arg2, arg3, arg4)
-		assert crawler.address == '195.218.195.228'
+		assert crawler.subnet == '192.168.60.254/24'
+
+	#def test_constructor_port_argument(self):
+		#pass
