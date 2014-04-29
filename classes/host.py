d5ead0c20f4e154f3f5104ec2f1e5f1b630a5aaf
logan <logan@Latitude.(none)>
2014-04-29 18:15:55 +0000

    added functions to return type of host

--- classes/host.py
+++ classes/host.py
@@ -10,13 +10,27 @@ class Host:
 		self.type = ''
 		self.name = ''			# hostname
 		self.visited = False	# has this host been checked or not
-		self.interface = 0		# current interface host is listening onll
+		self.interface = None	# current interface host is listening on
 		self.neighbors = []		# hosts connected to this host
-		#self.if_descr = ''
-		self.ent_table_nr = 0
+		self.if_descr = ''
+		self.ent_table_nr = None
 		self.vlan_id = ''
 		self.port_list = {}
 
 		self.if_table = {}
 		self.serial_number = ''
-		self.responds = True
+		self.model = ''
+		self.responds = True	# is the host answering snmp requests
+		self.types = []
+
+
+	def is_switch(self):
+		"""Return true if host is a switch"""
+
+		return 'switch' in self.types
+
+
+	def is_router(self):
+		"""Return true if host is a router"""
+
+		return 'router' in self.types
