ddb224c5c36e9ab888de432bdc64a69c3ee98293
logan <logan@Latitude.(none)>
2014-05-04 12:12:36 +0000

    added list of known and unknown ips and a few helper functions for them

--- classes/subnet.py
+++ classes/subnet.py
@@ -8,12 +8,15 @@ class Subnet:
 		self.broadcast = net.broadcast()
 		self.network = net.network()
 		
-		self.unknown_hosts = []			#not in use
-		self.known_hosts = []			#not in use
+		self.unknown_ips = []			#not in use
+		self.known_ips = []			#not in use
 		self.host_list = []
+		self.scanned = False
+
 
 	def __str__(self):
 		return str(self.id)
+
 
 	def in_network(self, address):
 		return self.net.in_network(address)
@@ -32,3 +35,28 @@ class Subnet:
 
 		if not self.host_exists(host):
 			self.host_list.append(host)
+
+
+	def add_known_ip(self, ip_addr):
+
+		if ip_addr not in self.known_ips:
+			self.known_ips.append(ip_addr)
+
+
+	def add_unknown_ip(self, ip_addr):
+
+		if ip_addr not in self.known_ips:
+			if ip_addr not in self.unknown_ips:
+				self.unknown_ips.append(ip_addr)
+
+
+	def remove_unknown_ip(self, ip_addr):
+
+		if ip_addr in self.unknown_ips:
+			self.unknown_ips.remove(ip_addr)
+
+
+	def get_next_unknown_ip(self):
+
+		if self.unknown_ips:
+			return self.unknown_ips[0]
