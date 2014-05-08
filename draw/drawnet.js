95a0ac3379675a61ebdc70fb8415d8f171e8a540
Theragon <theragon@Latitude.(none)>
2014-05-08 18:55:51 +0000

    node popover

--- draw/drawnet.js
+++ draw/drawnet.js
@@ -1,59 +1,96 @@
-var width = 1000,
-    height = 600;
 
-//var color = d3.scale.category20();
+var width = 1500,
+    height = 750,
+    fill = d3.scale.category20();
 
-var linkNormal = 4,
-    linkBold = 6,
-    nodeNormal = 14,
-    nodeBold = 17;
+var linkNormal = 2,
+    linkBold = 4,
+    nodeNormal = 5,
+    nodeBold = 8,
+    current;
 
-var force = d3.layout.force()
-    .charge(-500)
-    .linkDistance(250)
-    .theta(0.1)
-    .gravity(0.05)
-    .size([width, height]);
+var vis = d3.select("#network")
+    .append("svg:svg")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("pointer-events", "all")
+    .append('svg:g')
+      .call(d3.behavior.zoom().on("zoom", redraw))
+    .append('svg:g');
 
-var svg = d3.select("#network").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+vis.append('svg:rect')
+    .attr('width', width)
+    .attr('height', height)
+    .attr('fill', 'white');
 
-d3.json("net.json", function(error, graph) {
+function redraw() {
+  //console.log("here", d3.event.translate, d3.event.scale);
+  vis.attr("transform",
+      "translate(" + d3.event.translate + ")"
+      + " scale(" + d3.event.scale + ")");
+}
 
-  var select;
+function createHTML(d){
 
-  force
-      .nodes(graph.nodes)
-      .links(graph.links)
-      .start();
+		var ul = '<ul class="list-group">';
+    var li1 = '<li class="list-group-item"><strong>Name: </strong><span id="name">'+d.name+'</span></li>';
+    var li2 = '<li class="list-group-item"><strong>Serial number: </strong><span id="serial">'+d.serial+'</span></li>';
+    var li3 = '<li class="list-group-item"><strong>Model number: </strong><span id="model">'+d.model+'</span></li>';
+              
+      var ulend= '';//'</ul>';
+      var dd = '<li class="list-group-item"><div id="dd'+d.id+'" class="btn-group"><button type="button" class="btn btn-default dropdown-toggle macs" data-toggle="dropdown">MAC addresses</button><ul id="host0ul" class="dropdown-menu">';
 
-  var link = svg.selectAll(".link")
-      .data(graph.links)
-      .enter()
-      .append("line")
-      .attr("class", "link")
-      .style("stroke-width", linkNormal);
+      console.log('createHTML');
+      for(var i=0; i<d.macs.length; i++)
+      {
+      	var limac = '<li><a href="#">'+d.macs[i]+'</a></li>';
+      	dd += limac;
+      	console.log(d.macs[i]);
+      }
+      dd += '</ul></ul>';
 
-  var current;
+      return ul+li1+li2+li3+ulend+dd;
+	}
 
-  var div = d3.select("body").append("div")   
-    .attr("class", "tooltip")               
-    .style("opacity", 0);
+function draw(json) {
+  var force = d3.layout.force()
+    .charge(-120)
+    .linkDistance(30)
+    .nodes(json.nodes)
+    .links(json.links)
+    .size([width, height])
+    .start();
 
-  var node = svg.selectAll(".node")
-      .data(graph.nodes)
-      .enter().append("circle")
-      .attr("class", "node")
-      .attr("r", nodeNormal)
-      .style("fill", function(d) {return typeColoring(d.type, d.types);})
-      .call(force.drag)
-      .on('click', function(d){
-        d3.select(current).style("fill", function(d) { return typeColoring(d.type, d.types); }).transition().attr("r", nodeNormal)
-        .duration(200);
-        if(current != this) {
+  var link = vis.selectAll("line.link")
+    .data(json.links)
+    .enter().append("svg:line")
+    .attr("class", "link")
+    .style("stroke-width", linkNormal)
+    .attr("x1", function(d) { return d.source.x; })
+    .attr("y1", function(d) { return d.source.y; })
+    .attr("x2", function(d) { return d.target.x; })
+    .attr("y2", function(d) { return d.target.y; });
+
+  var node = vis.selectAll("circle.node")
+    .data(json.nodes)
+    .enter().append("svg:circle")
+    .attr("class", "node")
+    .attr("rel", "popover")
+			.attr("data-original-title", function(d){return '<div class="panel-heading"><b>Host </b>'+d.id+'</div>'})
+			.attr("data-content", function(d){return createHTML(d)})
+		.attr("id", function(d,i){return d.id})
+    .attr("cx", function(d) { return d.x; })
+    .attr("cy", function(d) { return d.y; })
+    .attr("r", nodeNormal)
+    .style("fill", function(d) {return typeColoring(d.type, d.types);})
+    .call(force.drag)
+    .on('click', function(d){
+      d3.select(current).style("stroke", function(d) { return typeColoring(d.type, d.types); })
+      .transition().attr("r", nodeNormal)
+      .duration(200);
+      if(current != this) {
           current = this;
-          d3.select(this).style("fill", "#E98931")
+          d3.select(this).style("stroke", "black")
           .transition()
           .attr("r", nodeBold)
           .duration(200);
@@ -71,131 +108,13 @@ d3.json("net.json", function(error, graph) {
           $("#serial").text("");
           $("#type").text("");
         }
-      });
+    });
+      
+  vis.style("opacity", 1e-6)
+    .transition()
+    .duration(1000)
+    .style("opacity", 1);
 
-  var linkpaths = svg.selectAll(".linkpath")
-      .data(graph.links)
-      .enter()
-      .append('path')
-      .attr({'d': function(d) {return 'M '+d.source.x+' '+d.source.y+' L '+ d.target.x +' '+d.target.y},
-             'class':'edgelabel',
-             'fill-opacity':0,
-             'stroke-opacity':0,
-             'fill':'blue',
-             'stroke':'red',
-             'id':function(d,i) {return 'linklabel'+i}})
-      .style("pointer-events", "none");
-
-  var linklabels = svg.selectAll(".linklabel")
-        .data(graph.links)
-        .enter()
-        .append('text')
-        .style("pointer-events", "none")
-        .attr('fill','transparent')
-        .attr({'class':'linklabel',
-               'id':function(d,i){return 'linklabel'+i},
-               'dx':80,
-               'dy':0,
-               'font-size':14,});
-
-  linklabels.append('textPath')
-        .data(graph.links)
-        .attr('xlink:href',function(d,i) {return '#linklabel'+i})
-        .style("pointer-events", "none")
-        .text(function(d){return d.sport + "-" + d.tport});
-
-  function parseType(type, types) {
-    if(type === "end_device")
-      return "end device";
-    else if(type === "networking") {
-      if(types.length === 1) 
-        return types[0];
-      else if(types.length === 2) {
-        if((types[0] === "router" && types[1] === "switch") || (types[0] === "switch" && types[1] === "router"))
-          return "router/switch"
-        else
-          return "unknown";
-      }
-    }
-    else
-      return "unknown";
-  }
-
-  function typeColoring(type, types) {
-
-    if(type === "end_device") 
-      return "#AA40FF";
-    else if(types.length === 1) {
-      if(types[0] === "router") 
-        return "#1F5EA8";      
-      else if(types[0] === "switch") 
-        return "#39C0B3";
-      else // Unknown type
-      return "#EB403B";
-    }
-    else if(types.length === 2) {
-      if((types[0] === "router" && types[1] === "switch") || (types[0] === "switch" && types[1] === "router"))
-        return "#719207"
-      else // Unknown type
-        return "#EB403B";
-    }
-    else // Unknown type
-      return "#EB403B";
-/*  
-    #E98931
-    #EB403B
-    #B32E37
-    #6C2A6A
-    #5C4399
-    #274389
-    #1F5EA8
-    #227FB0
-    #2AB0C5
-    #39C0B3
-*/
-  }
-
-  force.on("tick", function() {
-    link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
-
-    node.attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
-
-    
-
-    linkpaths.attr('d', function(d) { var path='M '+d.source.x+' '+d.source.y+' L '+ d.target.x +' '+d.target.y;
-                                       //console.log(d)
-                                       return path});       
-
-    /*linklabels.select("text").text(function(d){
-        if (d.target.x<d.source.x){
-            bbox = this.getBBox();
-            rx = bbox.x+bbox.width/2;
-            ry = bbox.y+bbox.height/2;
-            return parseInt(d.port2) + "-" + parseInt(d.port1);
-            }
-        else {
-            return parseInt(d.port1) + "-" + parseInt(d.port2);
-            }
-     });*/
-
-    /*linklabels.attr('transform',function(d){
-        if (d.target.x<d.source.x){
-            bbox = this.getBBox();
-            rx = bbox.x+bbox.width/2;
-            ry = bbox.y+bbox.height/2;
-            return 'rotate(180 '+rx+' '+ry+')';
-            }
-        else {
-            //linklabels.text(function(d){return parseInt(d.port1) + "-" + parseInt(d.port2)});
-            return 'rotate(0)';
-            }
-     });*/
-  });
-  
   node.on('mouseover', function(d) {
     link.style('stroke-width', function(l) {
       if (d === l.source || d === l.target)
@@ -215,34 +134,80 @@ d3.json("net.json", function(error, graph) {
   node.on('mouseout', function() {
     link.style('stroke-width', linkNormal);
     div.transition()        
-                .duration(500)      
-                .style("opacity", 0);
+    .duration(500)      
+    .style("opacity", 0);
 
   });
 
+  $(".node").popover({
+			placement: 'top',
+			html: true,
+			container:'body'
+		});
+
   link.on('mouseover', function() {
     d3.select(this).style("stroke-width", linkBold);
-      //.append("text")
-      //.text(function(d) {return d.port1;});
   });
 
   link.on('mouseout', function() {
     link.style("stroke-width", linkNormal);
   });
 
-  var showPortNbrs = false;
+  force.on("tick", function() {
+      link.attr("x1", function(d) { return d.source.x; })
+          .attr("y1", function(d) { return d.source.y; })
+          .attr("x2", function(d) { return d.target.x; })
+          .attr("y2", function(d) { return d.target.y; });
 
-  $('#showport').click( function() {
-    $(this).text(function(i, text){
-          return text === "Show Ports" ? "Hide Ports" : "Show Ports";
-      })
-    if(!showPortNbrs) {
-      linklabels.attr('fill', '#000');
-      showPortNbrs = true;
-    }
-    else {
-      linklabels.attr('fill', 'transparent');
-      showPortNbrs = false;
-    }
+      node.attr("cx", function(d) { return d.x; })
+          .attr("cy", function(d) { return d.y; });
   });
+
+  var div = d3.select("body").append("div")   
+    .attr("class", "tooltip")               
+    .style("opacity", 0);
+};
+
+function parseType(type, types) {
+  if(type === "end_device")
+    return "end device";
+  else if(type === "networking") {
+    if(types.length === 1) 
+      return types[0];
+    else if(types.length === 2) {
+      if((types[0] === "router" && types[1] === "switch") || (types[0] === "switch" && types[1] === "router"))
+        return "router/switch"
+      else
+        return "unknown";
+    }
+  }
+  else
+    return "unknown";
+}
+
+function typeColoring(type, types) {
+  if(type === "end_device") 
+    return "#AA40FF";
+  else if(types.length === 1) {
+    if(types[0] === "router") 
+      return "#1F5EA8";      
+    else if(types[0] === "switch") 
+      return "#39C0B3";
+    else // Unknown type
+    return "#EB403B";
+  }
+  else if(types.length === 2) {
+    if((types[0] === "router" && types[1] === "switch") || (types[0] === "switch" && types[1] === "router"))
+      return "#719207"
+    else // Unknown type
+      return "#EB403B";
+  }
+  else // Unknown type
+    return "#EB403B";
+}
+
+$( document ).ready(function() {
+    $.getJSON("net.json", function(json) {
+      draw(json);
+    });
 });
